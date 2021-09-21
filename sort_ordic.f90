a2b404ad7a3b5e880c6c992c1157c975961240e9
badubko <badubko@gmail.com>
2021-09-20 22:29:13 -0300

    Prueba evolutiva
    No muestra correctamente los decimales

--- sort_ordic.f90
+++ sort_ordic.f90
@@ -1,6 +1,7 @@
 MODULE declarations_ordic
 INTEGER (kind=8), PARAMETER ::samples_per_line=1    ! Number of samples/line
-INTEGER (kind=8), PARAMETER ::total_lines=20    ! Total lines
+INTEGER (kind=8), PARAMETER ::total_lines=20       ! Total lines
+INTEGER (kind=8), PARAMETER ::max_values=samples_per_line*total_lines   ! Total lines 
 
 REAL(kind=8)	    :: random_n (samples_per_line * total_lines)
 
@@ -9,6 +10,31 @@ END MODULE declarations_ordic
 ! ***********************************************************
 SUBROUTINE my_sort ()
 USE declarations_ordic
+
+INTEGER (kind=8)  :: I , J , K , L
+INTEGER (kind=8)  :: tmp_val
+LOGICAL           :: XCHANGE=.TRUE.
+
+DO WHILE (XCHANGE .EQV. .TRUE. )
+    XCHANGE=.FALSE.
+    DO I= 1, max_values-1, 2
+        IF (random_n(I) .GT. random_n(I+1) ) THEN
+            tmp_val= random_n(I)
+            random_n(I)=random_n(I+1)
+            random_n(I+1)=tmp_val
+            XCHANGE=.TRUE.
+        END IF
+    END DO
+        
+    DO I= 2, max_values-2, 2
+        IF (random_n(I) .GT. random_n(I+1) ) THEN
+            tmp_val= random_n(I)
+            random_n(I)=random_n(I+1)
+            random_n(I+1)=tmp_val
+            XCHANGE=.TRUE.
+        END IF
+    END DO    
+END DO
 
 END SUBROUTINE my_sort
 
@@ -38,9 +64,15 @@ DO I = 1, total_lines
 !    WRITE (*,my_format_str_2)  random_n ( 1:samples_per_line )
 
  READ (* , my_format_str_2 ) random_n(I)  ! Won't work if (samples_per_line != 1) !!!
- WRITE (*,my_format_str_2)  random_n (I) 
+ WRITE (*,my_format_str_2)  random_n(I) 
+
 END DO
 
+PRINT *
 CALL my_sort()
+
+DO I = 1, total_lines
+    WRITE (*,my_format_str_2)  random_n(I) 
+END DO
 
 END PROGRAM sort_ordic    
